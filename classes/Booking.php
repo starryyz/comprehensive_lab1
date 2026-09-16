b0682afcd196fbfc8176220e5da93162211388f2
Jalissav <jalissaxval@gmail.com>
2026-09-16 16:29:35 -0400

    jalissa push

--- classes/Booking.php
+++ classes/Booking.php
@@ -1,1 +1,24 @@
 // jalissa
+<?
+
+class Bookings {
+    public $bookingID;
+    public $name;
+    public $email;
+    public $date;
+    public $time; 
+
+function storesBooking(){
+
+}
+function confirmBooking(){
+
+}
+function reserveSpot(){
+
+}
+}
+
+
+
+?>
--- classes/FitnessClass.php
+++ classes/FitnessClass.php
@@ -1,1 +1,26 @@
 // jalissa
+
+<?php
+
+
+class fitnessClass {
+    public $className; 
+    public $trainerName;
+    public $price;
+    public $maxCapacity;
+    public $availableSpots;
+
+    function isAvailable(){
+
+    }
+    function reduceSpots(){
+
+    }
+    function reserveSpot(){
+        
+    }
+}
+
+
+
+?>
