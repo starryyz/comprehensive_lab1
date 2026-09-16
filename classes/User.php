62eb38d3f630142d8e3f7c350b1ab5b4a0c13904
starryyz <starsv015@gmail.com>
2026-09-16 15:27:41 -0400

    user class updated

--- classes/User.php
+++ classes/User.php
@@ -1,1 +1,17 @@
-// star
+<?php
+    //star
+    class User{
+        protected string $name;
+        protected string $email;
+
+        public function __construct(string $name, string $email){
+            $this->name = $name;
+            $this->email = $email;
+        }
+
+        public function viewProfile(): string{
+            return "Name: {$this->name}, Email: {$this->email}";
+        }
+    }
+
+?>
