0c1d3f79c831823086913a684f9913e7a0c3a700
EllieVance <elliegvance68@gmail.com>
2026-09-17 11:32:06 -0400

    ellie push

--- classes/Member.php
+++ classes/Member.php
@@ -1,1 +1,20 @@
+<?php
 // ellie
+class Member{
+    protected string $name;
+    protected string $email;
+
+    public function __construct(string $name, string $email){
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function bookClass(): string{
+        return "Member {$this->name} booked a class.";
+    }
+
+    public function viewProfile(): string{
+        return "Name: {$this->name}, Email: {$this->email}";
+    }
+}
+?>
--- classes/Trainer.php
+++ classes/Trainer.php
@@ -1,1 +1,20 @@
+<?php
 // ellie
+class Trainer{
+    protected string $name;
+    protected string $email;
+
+    public function __construct(string $name, string $email){
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function leadClass(): string{
+        return "Trainer {$this->name} is leading the class.";
+    }
+
+    public function viewProfile(): string{
+        return "Name: {$this->name}, Email: {$this->email}";
+    }
+}
+?>
